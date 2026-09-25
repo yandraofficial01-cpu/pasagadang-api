94c952b501a16b2ad695bcdfca3728186995068e
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-25 10:32:10 +0000

    Update database.py

--- database.py
+++ database.py
@@ -2,12 +2,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL") # AMBIL DARI VERCEL
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL belum di set di Vercel ENV!")
+
+# Tambahin ssl_mode kalo belum ada di URL TiDB
+connect_args = {}
+if "tidbcloud.com" in DATABASE_URL:
+    connect_args["ssl"] = {"ssl": True}
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
     pool_recycle=300,
+    connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
