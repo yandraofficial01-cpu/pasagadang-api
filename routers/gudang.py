4595b8a03cfe5b3a4f7bcb12f8a852849659f3c8
yandraofficial01-cpu <yandraofficial01@gmail.com>
2026-09-25 01:21:25 +0000

    Update gudang.py

--- routers/gudang.py
+++ routers/gudang.py
@@ -1,1 +1,18 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from database import get_db
+import models, schemas
 
+router = APIRouter(prefix="/gudangs", tags=["Gudangs"])
+
+@router.post("/", response_model=schemas.GudangResponse)
+def create_gudang(payload: schemas.GudangCreate, db: Session = Depends(get_db)):
+    new_item = models.Gudang(**payload.model_dump())
+    db.add(new_item)
+    db.commit()
+    db.refresh(new_item)
+    return new_item
+
+@router.get("/", response_model=list[schemas.GudangResponse])
+def list_gudang(db: Session = Depends(get_db)):
+    return db.query(models.Gudang).filter(models.Gudang.is_active == True).all()
